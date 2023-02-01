cb2d7a79e7b171e22d62506dec06a99acaf42d4e
Thodoris Sotiropoulos <theosotr@aueb.gr>
2023-02-01 17:43:28 +0100

    [api-graph]: Subtyping is transitive

--- src/generators/api_graph.py
+++ src/generators/api_graph.py
@@ -157,14 +157,12 @@ class APIGraph():
         excluded_nodes = set()
         for k, v in nx.dfs_edges(self.subtyping_graph,
                                  TypeNode(node.t.t_constructor)):
-            if k in excluded_nodes:
-                continue
             constraint = self.subtyping_graph[k][v].get("constraint") or {}
             if not constraint:
                 subtypes.add(v)
             solution = self.solve_constraint(constraint,
                                              dict(type_var_map))
-            if not solution:
+            if not solution or k in excluded_nodes:
                 excluded_nodes.add(v)
                 continue
             type_var_map = solution
