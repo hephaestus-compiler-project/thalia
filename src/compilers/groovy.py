5ba728035d1c432d3e5edc98ed945960f0878714
Thodoris Sotiropoulos <theosotr@aueb.gr>
2023-05-03 17:25:20 +0200

    [core]: Fix the analysis of Groovy's compiler output

--- src/compilers/groovy.py
+++ src/compilers/groovy.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import re
 import os
 
@@ -19,13 +20,13 @@ class GroovyCompiler(BaseCompiler):
 
     @classmethod
     def get_compiler_version(cls):
-        return ['groovyc', '-version']
+        return ['groovyc-l', '-version']
 
     def get_compiler_cmd(self):
         extra_options = []
         if self.library_path:
             extra_options = ["-cp", self.library_path]
-        return ['groovyc', '--compile-static'] + extra_options + \
+        return ['groovyc-l', '--compile-static'] + extra_options + \
             [self.input_name]
 
     def get_filename(self, match):
@@ -34,8 +35,24 @@ class GroovyCompiler(BaseCompiler):
     def get_error_msg(self, match):
         return match[1]
 
+    def _analyze_compiler_output(self, output):
+        failed = defaultdict(list)
+        filtered_output = output
+        for p in self.filter_patterns:
+            filtered_output = re.sub(p, '', filtered_output)
+        matches = re.findall(self.ERROR_REGEX, filtered_output)
+        for match in matches:
+            filename = self.get_filename(match)
+            error_msg = self.get_error_msg(match)
+            failed[filename].append(error_msg)
+        crash_match = re.search(self.CRASH_REGEX, output)
+        if crash_match and not matches:
+            self.crash_msg = output
+            return None, []
+        return failed, matches
+
     def analyze_compiler_output(self, output):
-        failed, matches = super().analyze_compiler_output(output)
+        failed, matches = self._analyze_compiler_output(output)
         stack_overflow = re.search(self.STACKOVERFLOW_REGEX, output)
         if stack_overflow and not matches:
             self.crash_msg = output
